ad8bf88e2bc54b19e967a0b420bb658946dd087e
MilkyJug <matthew.q.duran@gmail.com>
2020-08-20 23:21:34 -0400

    h

---
+++ index.js
@@ -0,0 +1,17 @@
+// First we require the Discord.js library.
+const Discord = require("discord.js");
+
+// Now we require the module.
+const DIL = require("discord.js-image-logger");
+
+// Start a new Discord Client.
+const client = new Discord.Client();
+// Start the module with some custom options.
+DIL(client, {
+  method: "embed", // can be "embed" "link" or "image"
+  logChannel: "746192045089226828",
+  channels: ["746155669198995547"]
+})
+
+// Login the Client
+client.login("NzQ2MTg4ODgxMzE3MTk5OTEy.Xz8sjA.XeZVYug05SVwUB9SVWlGVmbEHy4");
